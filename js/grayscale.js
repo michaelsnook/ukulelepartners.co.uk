f05a743be03abaae995ad09a4ce6dca289b9a661
michaelsnook <michael.snook@gmail.com>
2015-05-27 13:17:02 -0400

    touchups

--- js/grayscale.js
+++ js/grayscale.js
@@ -38,7 +38,7 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 11,
+        zoom: 10,
 
         // The latitude and longitude to center the map (always required)
         center: new google.maps.LatLng(51.584424, -0.0132015), // Walthamstow, London
